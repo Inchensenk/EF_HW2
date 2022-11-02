537c461a980fea0e254c819e52e10a60c8cb4526
Inchensenk <aab8117@gmail.com>
2022-11-02 17:33:39 +0300

    123

--- EF_HW2/Entities/Student.cs
+++ EF_HW2/Entities/Student.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace EF_HW2.Entities
 {
     public class Student
     {
         public int Id { get; set; }
-
+        [Required]
         public string FirstName { get; set; } = null!;
-
+        [Required]
         public string LastName { get; set; } = null!;
-
+        [Required]
         public DateTime BirthDate { get; set; }
-
+        [Required]
         public string PhoneNumber { get; set; } = null!;
 
         public Card? Card { get; set; }
